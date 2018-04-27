8b4bed5ada269839301de4d9232b8adb2f3fe06a
Joao Antunes <joaofbantunes@gmail.com>
2018-04-27 20:15:40 +0100

    Adding ServicePointManager DNS configurations for V1 solution.

--- src/CodingMilitia.HttpClientNotFollowingRedirectsSample.Web/Startup.cs
+++ src/CodingMilitia.HttpClientNotFollowingRedirectsSample.Web/Startup.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
+using System.Net.Http;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
@@ -19,13 +21,16 @@ namespace CodingMilitia.HttpClientNotFollowingRedirectsSample.Web
 
             //old school version
             services.AddSingleton<TrackingHttpClientWrapper>();
+            //not the best place to put this but... it'll suffice
+            ServicePointManager.FindServicePoint(new Uri("http://httpstat.us/")).ConnectionLeaseTimeout = (int)TimeSpan.FromMinutes(1).TotalMilliseconds;
+            ServicePointManager.DnsRefreshTimeout = (int)TimeSpan.FromMinutes(1).TotalMilliseconds;
 
             //ASP.NET Core 2.1 version
             services.AddScoped<TrackingHttpClientWrapperV2>();
             services.AddHttpClient<TrackingHttpClientWrapperV2>()
                 .ConfigurePrimaryHttpMessageHandler(() =>
                 {
-                    return new System.Net.Http.HttpClientHandler
+                    return new HttpClientHandler
                     {
                         AllowAutoRedirect = false
                     };
